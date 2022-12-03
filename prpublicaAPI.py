9464f15754a19838687c59c4b34fbae12ddb656a
Ingramml <ingramml@gmail.com>
2022-12-02 22:01:26 -0800

    Updates API to all results

--- prpublicaAPI.py
+++ prpublicaAPI.py
@@ -1,32 +1,31 @@
+import json
 import requests
 import selenium
 import pandas as pd
-from bs4 import BeautifulSoup
+import os
 
-#Searches for non profits with name from search term
-searchterm = 'Police'
+#Searches for non-profits with name from search term and exports file
+searchterm = 'PREGNANCY CENTER'
+target_location='/Volumes/SSD/TPC990/'
 x = requests.get('https://projects.propublica.org/nonprofits/api/v2/search.json?q='+searchterm)
 x=x.json()
-x=x['organizations']
-df=pd.DataFrame.from_dict(x)
-
-
-url = 'https://projects.propublica.org/nonprofits/search'
-
-r = requests.get(url)
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
-print(soup.prettify())
-
-
-
-
-
-
-print(r.content)
-#print(soup.prettify())
-
-
+page_num=x["num_pages"]
+print(page_num)
+z=0
+li=[]
+while z<page_num:
+    temprequest='https://projects.propublica.org/nonprofits/api/v2/search.json?q='+searchterm+'&?cur_page='+str(z)
+    newrequest=temprequest.replace(' ','%20')
+    x = requests.get(newrequest)
+    x=x.json()
+    x=x['organizations']
+    df = pd.DataFrame.from_dict(x)
+    #df = pd.read_csv(df2, index_col=None, header=0, low_memory=False, dtype="object")
+    li.append(df)
+    #print(li)
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    z = z + 1
+frame.to_csv(os.path.join(target_location,searchterm)+'_combined.csv', index=False)
+print(frame['ein'])
 
 
